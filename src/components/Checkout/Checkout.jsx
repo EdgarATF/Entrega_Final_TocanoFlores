beef3f2d5b2b23fd242bcc653b8f01013d685130
EdgarATF <edgar.alejandro1701@gmail.com>
2023-08-10 13:12:25 -0600

    final

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -4,7 +4,7 @@ import { db } from "../../firebase/config"
 import { addDoc, collection,updateDoc,doc, getDoc, writeBatch, where, documentId, query, getDocs } from "firebase/firestore"
 import { Link, Navigate } from "react-router-dom"
 import { Formik, Form, Field, ErrorMessage } from "formik"
-import { SummaryTable } from "../summarytable/summaryTable"
+import { SummaryTable } from "../SummaryTable/SummaryTable"
 import * as Yup from "yup"
 
 
